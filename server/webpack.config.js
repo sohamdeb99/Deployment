e5fb5380b5afbf25c8d998f521211b73ef708ff6
sohamdeb99 <118478834+sohamdeb99@users.noreply.github.com>
2024-05-04 03:34:12 +0100

    lm

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -2,7 +2,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './project/src/index.js',
+  entry: '/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
