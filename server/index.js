f1897aae56238c39cdd8d0faa9521ed0ecfb26d0
sohamdeb99 <118478834+sohamdeb99@users.noreply.github.com>
2024-05-04 03:29:42 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOMServer from 'react-dom/server';
 import App from './Landing Page/App';
+import fs from 'fs';
+import path from 'path';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Generate the HTML content by rendering the App component to a string
+const appHtml = ReactDOMServer.renderToString(<App />);
+
+// You might load an HTML file as a template or simply use a string template
+const template = fs.readFileSync(path.resolve(__dirname, 'template.html'), 'utf8');
+
+// Insert the rendered app into your template
+const html = template.replace('<div id="root"></div>', `<div id="root">${appHtml}</div>`);
+
+// Write the HTML to a file or send it over the network
+fs.writeFileSync(path.resolve(__dirname, 'output.html'), html);
